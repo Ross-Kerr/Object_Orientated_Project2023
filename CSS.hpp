cb78fb064cfaf0b62f795889f77e93428515b6af
Rossk <rosskerr2405@hotmail.co.uk>
2023-02-20 12:40:28 +0000

    Created Index header generator and hard coded the body generator.

--- CSS.hpp
+++ CSS.hpp
@@ -14,5 +14,40 @@ public:
 
 };
 
-
+//* Set font properties */
+//body {
+//    font-family: Arial, sans-serif;
+//    font-size: 16px;
+//    background-color: aliceblue;
+//  }
+//
+//  header{
+//    background-color: darkcyan;
+//  }
+//  /* Set link styles */
+//  a {
+//    color: #3366cc;
+//    text-decoration: none;
+//  }
+//
+//  a:hover {
+//    text-decoration: underline;
+//  }
+//
+//  /* Set list styles */
+//  ul {
+//    list-style-type: none;
+//    padding: 0;
+//    margin: 0;
+//  }
+//
+//  li {
+//    margin-bottom: 10px;
+//  }
+//
+//  /* Add some spacing between links */
+//  li a {
+//    margin-right: 10px;
+//  }
+//
 #endif //OBJECT_ORIENTATED_PROJECT2023_CSS_HPP
