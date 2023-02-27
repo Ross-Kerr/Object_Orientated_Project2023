fabe7310182e611ded10168b3cab9c2bb0811916
Rossk <rosskerr2405@hotmail.co.uk>
2023-02-27 11:43:48 +0000

    Added testing namespace to main

--- main.cpp
+++ main.cpp
@@ -6,11 +6,31 @@
 #include "Website.hpp"
 #include "CSS.hpp"
 #include "Linked_Page.hpp"
+#include <vector>
+#include <regex>
+namespace Testing{
 
+    int testLinkedPage(){
+        Linked_Page p;
+        std::cout<<p.to_string();
+    }
+
+    int testIndexPage(){
+        Index i;
+        std::cout<<i.to_string();
+    }
+
+    int testCSS(){
+        CSS c;
+        std::cout<<c.generateCSS();
+    }
+
+
+}
 int main() {
 
-    Linked_Page p;
-    std::cout<<p.to_string();
+    std::cout<<Testing::testCSS;
+
 
 
     return 0;
